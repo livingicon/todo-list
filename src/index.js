118479f271292bd97345117025a8c37b3bdbe334
Josh Hancock <livingicon@gmail.com>
2022-06-16 14:34:48 -0400

    Add :root project card color variables, add basic project card styling, add test functions and event listeners for project name and the add to-do list item button

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-console.log("Working");
-
 //logic for adding project, adding todo, and editing them?
 
 const addProject = (function() {
 
   const addDefaultProject = function(e) {
-    const addProjectBtn = document.getElementById('addProjectBtn');
     const projects = document.getElementById('projects');
     const defaultProject = document.createElement('div');
+    const projectTitle = document.createElement('h3');
+    const addToDoBtn = document.createElement('button');
+
+    projectTitle.addEventListener('dblclick', test);
+    addToDoBtn.addEventListener('click', test2);
 
     defaultProject.setAttribute('id', 'project'); //use backticks to make id project1?
-    defaultProject.textContent = "PROJECT";
+    projectTitle.setAttribute('id', 'projectTitle')
+    projectTitle.textContent = "Project Name";
+    addToDoBtn.setAttribute('id', 'addToDoBtn');
+    addToDoBtn.textContent = "+ add to-do list item";
 
     projects.appendChild(defaultProject);
+    defaultProject.appendChild(projectTitle);
+    defaultProject.appendChild(addToDoBtn);
   }
+
+  const test = function() {
+    console.log("double click");
+  };
+
+  const test2 = function() {
+    console.log("add a todo item");
+  };
 
   return { addDefaultProject };
 
