595ee02d151a2fc882a30e2c7da86b4c61d7b17b
Josh Hancock <livingicon@gmail.com>
2022-06-17 13:28:29 -0400

    Fix bug to only add project todo item button if one does not already exist

--- src/index.js
+++ src/index.js
@@ -1,52 +1,70 @@
 //logic for adding project, adding todo, and editing them?
 
 const addProject = (function() {
-
+  //should I put all of the const up here for all below functions?
+  
   const addDefaultProject = function(e) {
     const projects = document.getElementById('projects');
     const defaultProject = document.createElement('div');
-    const projectTitleInput = document.createElement('input');
-
-    projectTitleInput.addEventListener('keypress', editProjectName);
-
     defaultProject.setAttribute('id', 'defaultProject'); //use backticks to make id project1?
-    projectTitleInput.setAttribute('id', 'projectTitleInput')
-    projectTitleInput.setAttribute('type', 'text');
-    projectTitleInput.setAttribute('placeholder', 'Project Name')
-
     projects.appendChild(defaultProject);
-    defaultProject.appendChild(projectTitleInput);
+    addProjectTitleInput();
   }
 
-  const editProjectName = function(e) { 
+  function addProjectTitleInput() {
+    const projectTitleInput = document.createElement('input');
+    projectTitleInput.addEventListener('keypress', inputProjectName);
+    projectTitleInput.setAttribute('id', 'projectTitleInput')
+    projectTitleInput.setAttribute('type', 'text');
+    projectTitleInput.setAttribute('placeholder', 'Project Name');
+    //cut if statement out to own function?
+    if (defaultProject.firstChild === null){
+      defaultProject.appendChild(projectTitleInput);
+    } else if (defaultProject.firstChild !== null) {
+      projectTitleInput.removeAttribute('placeholder');
+      projectTitleInput.setAttribute('value', `${projectTitle.innerHTML}`);
+      defaultProject.replaceChild(projectTitleInput, projectTitle);
+    }
+  };
+
+  const inputProjectName = function(e) { 
     if(e.key === "Enter") {
       const projectTitle = document.createElement('h3');
       projectTitle.setAttribute('id', 'projectTitle');
       projectTitle.innerHTML = `${e.target.value}`;
       defaultProject.replaceChild(projectTitle, projectTitleInput);
+      projectTitle.addEventListener('dblclick', addProjectTitleInput);
       addToDoBtn();
     }
   };
 
   const addToDoBtn = function() {
-    const addToDoBtn = document.createElement('button');
-    addToDoBtn.addEventListener('click', addToDoItem);
-    addToDoBtn.setAttribute('id', 'addToDoBtn');
-    addToDoBtn.textContent = "+ add project to-do list item";
-    defaultProject.appendChild(addToDoBtn);
+    if(document.getElementById('addToDoBtn') === null) {
+      const addToDoBtn = document.createElement('button');
+      addToDoBtn.addEventListener('click', addToDoItem);
+      addToDoBtn.setAttribute('id', 'addToDoBtn');
+      addToDoBtn.textContent = "+ add project to-do list item";
+      defaultProject.appendChild(addToDoBtn);
+    }
   };
 
   const addToDoItem = function() {
     const toDoItem = document.createElement('input');
-    //open modal
+    //expand
     console.log("add a todo item");
   };
 
-  return { addDefaultProject };
 
+
+
+
+
+
+  return { addDefaultProject };
 })();
 
 const addProjectBtn = document.getElementById('addProjectBtn');
 addProjectBtn.addEventListener('click', addProject.addDefaultProject);
 
 // need to separate out the change from h3 to input into different modules
+// when project name added it needs to go to sidebar
