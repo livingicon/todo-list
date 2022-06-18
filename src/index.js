e94d0f25790a87f54e716d6730c171b23f01c76a
Josh Hancock <livingicon@gmail.com>
2022-06-18 17:32:06 -0400

    Change titleInput to be a form with a div and add input form for the addToDoListItem button event

--- src/index.js
+++ src/index.js
@@ -11,28 +11,42 @@ const addProject = (function() {
     addProjectTitleInput();
   }
 
-  function addProjectTitleInput() {
+  function addProjectTitleInput(e) {
+    const projectTitleForm = document.createElement('div');
+    const projectTitleLabel = document.createElement('label');
     const projectTitleInput = document.createElement('input');
+    projectTitleForm.setAttribute('id', 'projectTitleForm');
+    projectTitleLabel.setAttribute('id', 'projectTitleLabel');
     projectTitleInput.addEventListener('keypress', inputProjectName);
-    projectTitleInput.setAttribute('id', 'projectTitleInput')
+    projectTitleLabel.setAttribute('for', 'projectTitleInput');
+    projectTitleLabel.textContent = "Project Name:";
+    projectTitleInput.setAttribute('id', 'projectTitleInput');
     projectTitleInput.setAttribute('type', 'text');
     projectTitleInput.setAttribute('placeholder', 'Project Name');
-    //cut if statement out to own function?
+
     if (defaultProject.firstChild === null){
-      defaultProject.appendChild(projectTitleInput);
+      defaultProject.appendChild(projectTitleForm);
+      projectTitleForm.appendChild(projectTitleLabel);
+      projectTitleForm.appendChild(projectTitleInput);
     } else if (defaultProject.firstChild !== null) {
+      console.log(e);
       projectTitleInput.removeAttribute('placeholder');
       projectTitleInput.setAttribute('value', `${projectTitle.innerHTML}`);
-      defaultProject.replaceChild(projectTitleInput, projectTitle);
+
+      defaultProject.replaceChild(projectTitleForm, projectTitle);
+      projectTitleForm.appendChild(projectTitleLabel);
+      projectTitleForm.appendChild(projectTitleInput);
+      
     }
   };
 
   const inputProjectName = function(e) { 
     if(e.key === "Enter") {
       const projectTitle = document.createElement('h3');
+      projectTitleLabel.remove();
       projectTitle.setAttribute('id', 'projectTitle');
       projectTitle.innerHTML = `${e.target.value}`;
-      defaultProject.replaceChild(projectTitle, projectTitleInput);
+      defaultProject.replaceChild(projectTitle, projectTitleForm);
       projectTitle.addEventListener('dblclick', addProjectTitleInput);
       addToDoBtn();
     }
@@ -43,15 +57,87 @@ const addProject = (function() {
       const addToDoBtn = document.createElement('button');
       addToDoBtn.addEventListener('click', addToDoItem);
       addToDoBtn.setAttribute('id', 'addToDoBtn');
-      addToDoBtn.textContent = "+ add project to-do list item";
+      addToDoBtn.textContent = "+ add to-do item";
       defaultProject.appendChild(addToDoBtn);
     }
   };
 
   const addToDoItem = function() {
-    const toDoItem = document.createElement('input');
-    //expand
-    console.log("add a todo item");
+    const addToDoBtn = document.getElementById('addToDoBtn');
+    const toDoForm = document.createElement('form');
+    toDoForm.setAttribute('id', 'toDoForm');
+
+    const toDoFormTitleDiv = document.createElement('div');
+    const toDoFormTitleLabel = document.createElement('label');
+    const toDoFormTitleInput = document.createElement('input');
+    toDoFormTitleLabel.setAttribute('for', 'title');
+    toDoFormTitleLabel.textContent = "Title";
+    toDoFormTitleInput.setAttribute('type', 'text');
+    toDoFormTitleInput.setAttribute('id', 'title');
+    toDoFormTitleInput.setAttribute('placeholder', 'title')
+
+    const toDoFormDescriptionDiv = document.createElement('div');
+    const toDoFormDescriptionLabel = document.createElement('label');
+    const toDoFormDescriptionInput = document.createElement('input');
+    toDoFormDescriptionLabel.setAttribute('for', 'descripton');
+    toDoFormDescriptionLabel.textContent = "Description";
+    toDoFormDescriptionInput.setAttribute('type', 'text');
+    toDoFormDescriptionInput.setAttribute('id', 'descripton');
+    toDoFormDescriptionInput.setAttribute('placeholder', 'description')
+
+    const toDoFormDueDateDiv = document.createElement('div');
+    const toDoFormDueDateLabel = document.createElement('label');
+    const toDoFormDueDateInput = document.createElement('input');
+    toDoFormDueDateLabel.setAttribute('for', 'due-date');
+    toDoFormDueDateLabel.textContent = "Due Date";
+    toDoFormDueDateInput.setAttribute('type', 'date');
+    toDoFormDueDateInput.setAttribute('id', 'due-date');
+
+    const toDoFormPriorityDiv = document.createElement('div');
+    const toDoFormPriorityLabel = document.createElement('label');
+    const toDoFormPrioritySelect = document.createElement('select');
+    const priorityNow = document.createElement('option');
+    const prioritySoon = document.createElement('option');
+    const priorityLater = document.createElement('option');
+    toDoFormPriorityLabel.setAttribute('for', 'priority')
+    toDoFormPriorityLabel.textContent = "Priority";
+    toDoFormPrioritySelect.setAttribute('id', 'priority');
+    priorityNow.setAttribute('value', 'urgent');
+    priorityNow.setAttribute('id', 'priorityNow');
+    priorityNow.textContent = "Urgent";
+    priorityNow.style.backgroundColor = 'red';
+    prioritySoon.setAttribute('value', 'soon');
+    prioritySoon.setAttribute('id', 'prioritySoon');
+    prioritySoon.textContent = "Soon";
+    prioritySoon.style.backgroundColor = 'yellow';
+    priorityLater.setAttribute('value', 'later');
+    priorityLater.setAttribute('id', 'priorityLater');
+    priorityLater.textContent = "Later";
+    priorityLater.style.backgroundColor = 'green';
+
+    const toDoFormSaveBtn = document.createElement('button');
+    toDoFormSaveBtn.setAttribute('id', 'toDoSaveBtn');
+    toDoFormSaveBtn.setAttribute('type', 'submit');
+    toDoFormSaveBtn.textContent = "save";
+    
+    addToDoBtn.remove();
+    defaultProject.appendChild(toDoForm);
+    toDoForm.appendChild(toDoFormTitleDiv);
+    toDoFormTitleDiv.appendChild(toDoFormTitleLabel);
+    toDoFormTitleDiv.appendChild(toDoFormTitleInput);
+    toDoForm.appendChild(toDoFormDescriptionDiv);
+    toDoFormDescriptionDiv.appendChild(toDoFormDescriptionLabel);
+    toDoFormDescriptionDiv.appendChild(toDoFormDescriptionInput);
+    toDoForm.appendChild(toDoFormDueDateDiv);
+    toDoFormDueDateDiv.appendChild(toDoFormDueDateLabel);
+    toDoFormDueDateDiv.appendChild(toDoFormDueDateInput);
+    toDoForm.appendChild(toDoFormPriorityDiv);
+    toDoFormPriorityDiv.appendChild(toDoFormPriorityLabel);
+    toDoFormPriorityDiv.appendChild(toDoFormPrioritySelect);
+    toDoFormPrioritySelect.appendChild(priorityNow);
+    toDoFormPrioritySelect.appendChild(prioritySoon);
+    toDoFormPrioritySelect.appendChild(priorityLater);
+    defaultProject.appendChild(toDoFormSaveBtn);
   };
 
 
