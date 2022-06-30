ce63a8f73782a76d4f708cd339e0cd737a66037e
Josh Hancock <livingicon@gmail.com>
2022-06-30 18:23:34 -0400

    Add toDoArray to Project constructor and push todos to this array

--- src/addProjectForm.js
+++ src/addProjectForm.js
@@ -1,9 +1,56 @@
-import {addProject, addAllProjects, addToDoItem} from "./index.js";
+import {addProject, addAllProjects, addToDo} from "./index.js";
 
 const addForms = (function() {
 
+  // ADD PROJECT FORM FUNCTION
+  const addProjectForm = function(e) {
+    // get
+    const projects = document.getElementById('projects');
+    // create
+    const projectForm = document.createElement('form');
+    const projectFormDiv = document.createElement('div');
+    const projectFormLabel = document.createElement('label');
+    const projectFormInput = document.createElement('input');
+    const projectFormSaveBtn = document.createElement('button');
+    const projectFormCancelBtn = document.createElement('button');
+    // add attributes
+    projectForm.setAttribute('id', 'projectForm');
+    projectFormDiv.setAttribute('id', 'projectFormDiv');
+    projectFormLabel.setAttribute('for', 'project');
+    projectFormLabel.textContent = "Project Name";
+    projectFormInput.setAttribute('type', 'text');
+    projectFormInput.setAttribute('id', 'project');
+    projectFormInput.setAttribute('placeholder', 'project name')
+    projectFormSaveBtn.setAttribute('id', 'projectFormSaveBtn');
+    projectFormSaveBtn.setAttribute('type', 'submit');
+    projectFormSaveBtn.textContent = "save";
+    projectFormSaveBtn.style.backgroundColor = "var(--later)"
+    projectFormCancelBtn.setAttribute('id', 'projectCancelBtn');
+    projectFormCancelBtn.setAttribute('type', 'button');
+    projectFormCancelBtn.textContent = 'cancel';
+    projectFormCancelBtn.style.backgroundColor = "var(--urgent)"
+    // append
+    projects.innerHTML = ""; // removes projects so only form is visible
+    projects.appendChild(projectForm);
+    projectForm.appendChild(projectFormDiv)
+    projectFormDiv.appendChild(projectFormLabel);
+    projectFormDiv.appendChild(projectFormInput);
+    projectForm.appendChild(projectFormSaveBtn);
+    projectForm.appendChild(projectFormCancelBtn);
+    // listen
+    projectFormSaveBtn.addEventListener('click', addProject);
+    projectFormCancelBtn.addEventListener('click', addAllProjects);
+  };
+
+
+  // ADD TODO FORM FUNCTION
   const addToDoForm = function(e) {
-    // todo title
+    // --todo form
+    const projects = document.getElementById('projects');
+    const toDoForm = document.createElement('form');
+    const toDoFormDiv = document.createElement('div');
+    toDoForm.setAttribute('id', 'toDoForm');
+    // --todo title
     const toDoFormTitleDiv = document.createElement('div');
     const toDoFormTitleLabel = document.createElement('label');
     const toDoFormTitleInput = document.createElement('input');
@@ -12,7 +59,7 @@ const addForms = (function() {
     toDoFormTitleInput.setAttribute('type', 'text');
     toDoFormTitleInput.setAttribute('id', 'title');
     toDoFormTitleInput.setAttribute('placeholder', 'title')
-    // todo description
+    // --todo description
     const toDoFormDescriptionDiv = document.createElement('div');
     const toDoFormDescriptionLabel = document.createElement('label');
     const toDoFormDescriptionInput = document.createElement('input');
@@ -21,7 +68,7 @@ const addForms = (function() {
     toDoFormDescriptionInput.setAttribute('type', 'text');
     toDoFormDescriptionInput.setAttribute('id', 'description');
     toDoFormDescriptionInput.setAttribute('placeholder', 'description')
-    // todo due date
+    // --todo due-date
     const toDoFormDueDateDiv = document.createElement('div');
     const toDoFormDueDateLabel = document.createElement('label');
     const toDoFormDueDateInput = document.createElement('input');
@@ -29,7 +76,7 @@ const addForms = (function() {
     toDoFormDueDateLabel.textContent = "To Do Item Due Date";
     toDoFormDueDateInput.setAttribute('type', 'date');
     toDoFormDueDateInput.setAttribute('id', 'due-date');
-    // todo priority
+    // --todo priority
     const toDoFormPriorityDiv = document.createElement('div');
     const toDoFormPriorityLabel = document.createElement('label');
     const toDoFormPrioritySelect = document.createElement('select');
@@ -51,66 +98,44 @@ const addForms = (function() {
     priorityLater.setAttribute('id', 'priorityLater');
     priorityLater.textContent = "Later";
     priorityLater.style.backgroundColor = 'var(--later)';
-
+    // --todo save button
+    const toDoFormSaveBtn = document.createElement('button');
+    toDoFormSaveBtn.setAttribute('id', 'toDoFormSaveBtn');
+    toDoFormSaveBtn.setAttribute('type', 'submit');
+    toDoFormSaveBtn.textContent = "save";
+    toDoFormSaveBtn.style.backgroundColor = "var(--later)"
+    let position = e.target.getAttribute('data-position');
+    toDoFormSaveBtn.setAttribute('data-position', `${position}`);
+    // --todo cancel button
+    const toDoFormCancelBtn = document.createElement('button');
+    toDoFormCancelBtn.setAttribute('id', 'toDoCancelBtn');
+    toDoFormCancelBtn.setAttribute('type', 'button');
+    toDoFormCancelBtn.textContent = 'cancel';
+    toDoFormCancelBtn.style.backgroundColor = "var(--urgent)"
     // append
-    projectToDoForm.appendChild(toDoFormTitleDiv);
+    projects.innerHTML = ""; // removes projects so only form is visible
+    projects.appendChild(toDoForm);
+    toDoForm.appendChild(toDoFormDiv);
+    toDoFormDiv.appendChild(toDoFormTitleDiv);
     toDoFormTitleDiv.appendChild(toDoFormTitleLabel);
     toDoFormTitleDiv.appendChild(toDoFormTitleInput);
-    projectToDoForm.appendChild(toDoFormDescriptionDiv);
+    toDoFormDiv.appendChild(toDoFormDescriptionDiv);
     toDoFormDescriptionDiv.appendChild(toDoFormDescriptionLabel);
     toDoFormDescriptionDiv.appendChild(toDoFormDescriptionInput);
-    projectToDoForm.appendChild(toDoFormDueDateDiv);
+    toDoFormDiv.appendChild(toDoFormDueDateDiv);
     toDoFormDueDateDiv.appendChild(toDoFormDueDateLabel);
     toDoFormDueDateDiv.appendChild(toDoFormDueDateInput);
-    projectToDoForm.appendChild(toDoFormPriorityDiv);
+    toDoFormDiv.appendChild(toDoFormPriorityDiv);
     toDoFormPriorityDiv.appendChild(toDoFormPriorityLabel);
     toDoFormPriorityDiv.appendChild(toDoFormPrioritySelect);
     toDoFormPrioritySelect.appendChild(priorityNow);
     toDoFormPrioritySelect.appendChild(prioritySoon);
     toDoFormPrioritySelect.appendChild(priorityLater);
-    projectToDoForm.appendChild(toDoFormSaveBtn);
-    projectToDoForm.appendChild(toDoFormCancelBtn);
-  };
-
-  const addProjectForm = function(e) {
-    // let position = e.target.getAttribute('data-position');
-    const projects = document.getElementById('projects');
-    const projectForm = document.createElement('form');
-    const projectFormDiv = document.createElement('div');
-    const projectFormLabel = document.createElement('label');
-    const projectFormInput = document.createElement('input');
-    // attributes
-    projectForm.setAttribute('id', 'projectForm');
-    projectFormDiv.setAttribute('id', 'projectFormDiv');
-    projectFormLabel.setAttribute('for', 'project');
-    projectFormLabel.textContent = "Project Name";
-    projectFormInput.setAttribute('type', 'text');
-    projectFormInput.setAttribute('id', 'project');
-    projectFormInput.setAttribute('placeholder', 'project name')
-    // save button
-    const projectFormSaveBtn = document.createElement('button');
-    projectFormSaveBtn.setAttribute('id', 'projectFormSaveBtn');
-    projectFormSaveBtn.setAttribute('type', 'submit');
-    // toDoFormSaveBtn.setAttribute('data-position', `${position}`);
-    projectFormSaveBtn.textContent = "save";
-    projectFormSaveBtn.style.backgroundColor = "var(--later)"
-    // cancel button
-    const projectFormCancelBtn = document.createElement('button');
-    projectFormCancelBtn.setAttribute('id', 'projectCancelBtn');
-    projectFormCancelBtn.setAttribute('type', 'button');
-    projectFormCancelBtn.textContent = 'cancel';
-    projectFormCancelBtn.style.backgroundColor = "var(--urgent)"
-    // append
-    projects.innerHTML = ""; // removes projects so only form is visible
-    projects.appendChild(projectForm);
-    projectForm.appendChild(projectFormDiv)
-    projectFormDiv.appendChild(projectFormLabel);
-    projectFormDiv.appendChild(projectFormInput);
-    projectForm.appendChild(projectFormSaveBtn);
-    projectForm.appendChild(projectFormCancelBtn);
-
-    projectFormSaveBtn.addEventListener('click', addProject);
-    projectFormCancelBtn.addEventListener('click', addAllProjects);
+    toDoFormDiv.appendChild(toDoFormSaveBtn);
+    toDoFormDiv.appendChild(toDoFormCancelBtn);
+    // listen
+    toDoFormSaveBtn.addEventListener('click', addToDo);
+    toDoFormCancelBtn.addEventListener('click', addAllProjects);
   };
 
   return { addProjectForm, addToDoForm };
--- src/index.js
+++ src/index.js
@@ -19,41 +19,49 @@ let myProjects = []; //does this need to be here?
 
 // -----addProject File-----
 // convert constructor function to class?
-function Project(project, title, description, date, priority) {
+function Project(project) {
   this.project = project;
-  this.title = title;
-  this.description = description;
-  this.date = date;
-  this.priority = priority;
+  this.toDoArray = [];
 };
 
 const addProject = function(e){
   e.preventDefault();
   const project = document.getElementById('project').value;
-  const title = document.getElementById('title').value;
-  const description = document.getElementById('description').value;
-  const date = document.getElementById('due-date').value;
-  const priority = document.getElementById('priority').value;
-  if (!project || !title || !description || !date) {
-    alert("All fields are required to add a project and project todo item.");
+  if (!project) {
+    alert("Please enter a project name in the input.");
     return false;
   } else {
-    let newProject = new Project(project, title, description, date, priority);
+    let newProject = new Project(project);
     myProjects.push(newProject);
     localStorage.setItem('myProjects', JSON.stringify(myProjects));
     addAllProjects(newProject); 
   }
 };
 
-function ToDo(title, description, date, priority) {
-  this.title = title;
-  this.description = description;
-  this.date = date;
-  this.priority = priority;
+// function ToDo(title, description, date, priority) { //convert constructor function to class
+//   this.title = title;
+//   this.description = description;
+//   this.date = date;
+//   this.priority = priority;
+// };
+
+class ToDo {
+  constructor(
+    title = "unknown",
+    description = "unknown",
+    date = 0,
+    priority = 0
+  ) {
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.priority = priority;
+  }
 };
 
-const addToDoItem = function(e){
+const addToDo = function(e){ // objects can't have duplicate keys
   e.preventDefault();
+  //change name below based on if one exists
   const title = document.getElementById('title').value;
   const description = document.getElementById('description').value;
   const date = document.getElementById('due-date').value;
@@ -62,83 +70,86 @@ const addToDoItem = function(e){
     alert("All fields are required to add todo item.");
     return false;
   } else {
-    // replaces existing todo item (because title.this, etc? Need new form.)
-    const targetObj = myProjects[`${e.target.getAttribute('data-position')}`];
+    // let targetObj = myProjects[`${e.target.getAttribute('data-position')}`];
     let newToDo = new ToDo(title, description, date, priority);
-    const newObj = Object.assign(targetObj, newToDo);
-    console.log(targetObj);
-    addAllProjects();
-    // myProjects.push(newToDo); // push where?
-    // addNewTodo(newToDo); //change this to addNewTodo function? 
-    // localStorage.setItem('myProjects', JSON.stringify(myProjects));
-    // console.log(myProjects[`${location}`].date); //working
-    // let location = `${e.target.getAttribute('data-position')}`;
-  }
+    myProjects[`${e.target.getAttribute('data-position')}`].toDoArray.push(newToDo);
+    localStorage.setItem('myProjects', JSON.stringify(myProjects));
+    addAllProjects(newToDo);
+    addAllToDos();
+    // console.log(myProjects);
+    }
+  };
+
+const addAllToDos = function(){
+
 };
 
-// const addNewToDo = function(newToDo){
-
-// };
-
 // -----loadApp File-----
-const addAllProjects = function(newProject){
+//how can I make this add the todos as well?
+const addAllProjects = function(newProject, newToDo){
   const projects = document.getElementById('projects');
   const projectList = document.getElementById('projectList');
   projects.innerHTML = ""; //clears existing projects to avoid doubles
   projectList.innerHTML = "";
   myProjects.forEach(element => generateProjectCards(element));
+  console.log(myProjects);
+  console.log(myProjects[0].toDoArray[0].date); // working
 };
 
+//how do I cycle through the ToDoArray
 const generateProjectCards = function(stuff){
   const projectList = document.getElementById('projectList'); // sidebar list
   const projects = document.getElementById('projects'); // projects
-
   const card = document.createElement('div');
   const cardProject = document.createElement('div');
-  const cardToDoList = document.createElement('div');
   const projectTitle = document.createElement('h2');
   const projectTitleSidebar = document.createElement('h6');
-  const toDoListHeader = document.createElement('h4')
-  const toDoItem = document.createElement('h6');
   const addToDoBtn = document.createElement('button');
-
+  // attributes
   card.setAttribute('class', 'card');
   card.setAttribute('id', `${myProjects.indexOf(stuff)}`); //works
   projectTitle.setAttribute('id', 'projectTitle');
   projectTitle.textContent = `Project: ${stuff.project}`;
-  cardToDoList.setAttribute('id', 'cardToDoList')
-  toDoListHeader.setAttribute('id', 'toDoListHeader');
-  toDoListHeader.textContent = `${stuff.project} To Do List:`;
-  toDoItem.setAttribute('id', 'toDoItem');
-  toDoItem.innerHTML = `${stuff.title}: ${stuff.description}<br />
-  Goal Completion: ${stuff.date}`;
   addToDoBtn.setAttribute('id', 'addToDoBtn');
   addToDoBtn.setAttribute('type', 'submit');
   addToDoBtn.setAttribute('data-position', `${myProjects.indexOf(stuff)}`); // added data-position here
   addToDoBtn.textContent = "+add todo item";
   projectTitleSidebar.textContent = `${stuff.project}`;
   projectTitleSidebar.setAttribute('id', 'projectTitleSidebar');
-
+  // append
   projects.append(card);
   card.append(cardProject);
   cardProject.append(projectTitle);
-  card.append(cardToDoList);
-  cardToDoList.append(toDoListHeader);
-  cardToDoList.append(toDoItem);
-  cardToDoList.append(addToDoBtn);
-  //sidebar
-  projectList.append(projectTitleSidebar); //make event listeners
-
-  if (stuff.priority === "urgent") {
-    toDoItem.style.backgroundColor = 'var(--urgent)';
-  } else if (stuff.priority === "soon") {
-    toDoItem.style.backgroundColor = 'var(--soon)';
-  } else {
-    toDoItem.style.backgroundColor = 'var(--later)';
-  }
-
-  addToDoBtn.addEventListener('click', addForms.addProjectForm);
+  card.append(addToDoBtn);
+  // append sidebar
+  projectList.append(projectTitleSidebar); //make event listeners?
+  // event listeners
+  addToDoBtn.addEventListener('click', addForms.addToDoForm);
 };
+
+  // const cardToDoList = document.createElement('div');
+  // const toDoListHeader = document.createElement('h4')
+  // const toDoItem = document.createElement('h6');
+
+  // cardToDoList.setAttribute('id', 'cardToDoList')
+  // toDoListHeader.setAttribute('id', 'toDoListHeader');
+  // toDoListHeader.textContent = `${stuff.project} To Do List:`;
+  // toDoItem.setAttribute('id', 'toDoItem');
+  // toDoItem.innerHTML = `${stuff.title}: ${stuff.description}<br />
+  // Goal Completion: ${stuff.date}`;
+
+  // card.append(cardToDoList);
+  // cardToDoList.append(toDoListHeader);
+  // cardToDoList.append(toDoItem);
+
+  // if (stuff.priority === "urgent") {
+  //   toDoItem.style.backgroundColor = 'var(--urgent)';
+  // } else if (stuff.priority === "soon") {
+  //   toDoItem.style.backgroundColor = 'var(--soon)';
+  // } else {
+  //   toDoItem.style.backgroundColor = 'var(--later)';
+  // }
+
 
 
 
@@ -150,12 +161,10 @@ addProjectBtn.addEventListener('click', addForms.addProjectForm);
 export {
   addProject,
   addAllProjects,
-  addToDoItem
+  addToDo
 };
 
 // TO DO
-// 4. make form update todo on specific project (not what originally designed for)
-
 // - factory function?
 // - add title on sidebar
 // - make sidebar title clickable
@@ -169,3 +178,10 @@ export {
 // - break into modules (loadPage, addProject, editProject, addToDoItem);
 // - add projects header (All Projects)
 // - change "All Projects" to "show all projects"
+
+// Make your project constructor output something like this:
+// projectA = {
+//   name:  "Project A",
+//   todoArray: [],
+// }
+// Then when you create a todo that belongs in projectA you can push it into its array like projectA.todoArray.push(newTodo)
